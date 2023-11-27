121657ffb6e39f08e28a7511e405cfeeccb4693d
ibrahim oke <okeibrahim917@gmail.com>
2023-11-27 14:50:33 +0100

    getAllTask second commit

--- app.js
+++ app.js
@@ -63,27 +63,3 @@ app.get("/task/:id", async (req, res) => {
 });
 
 
-app.post("/tasks", async (req, res) => {
-  try {
-    const { title, isComplete } = req.body;
-    if (!(title && isComplete)) {
-      return res.status(400).json({ error: "Invalid task data" });
-    }
-
-    const newTask = {
-      id: tasks.length + 1,
-      title,
-      isCompleted: true,
-    };
-    tasks.push(newTask);
-    res.status(201).json({
-      success: true,
-      mesage: "a new task has been created",
-      task: newTask,
-    });
-  } catch (error) {}
-});
-
-app.listen(Port, () => {
-  console.log(`server running on ${Port}`);
-});
